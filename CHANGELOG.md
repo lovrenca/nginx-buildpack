8da240ce6cfb7eda75bb60c9954a7c39d44f0bd8
Lovrenc Avsenek <a.lovrenc@gmail.com>
2016-03-15 17:20:45 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,41 @@
+## v0.6
+
+2016-02-14
+
+* Update nginx to 1.9.11
+* Update PCRE to 8.38
+
+## v0.5
+
+2015-05-09
+
+* Update nginx to 1.8.0
+* add gzip static module
+
+## v0.4
+
+2012-05-13
+
+Update NGINX config
+
+* enabled gzip compression
+* using epoll and increasing workers to 4
+
+## v0.3
+
+2012-05-11
+
+* Improve process managment using a fifo.
+
+## v0.2
+
+2012-05-10
+
+* Improve the handling of app server failures
+
+## v0.1
+
+2012-05-09
+
+* NGINX 1.4.1
+* inital release
---
+++ scripts/build_nginx.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Build NGINX and modules on Heroku.
+# This program is designed to run in a web dyno provided by Heroku.
+# We would like to build an NGINX binary for the builpack on the
+# exact machine in which the binary will run.
+# Our motivation for running in a web dyno is that we need a way to
+# download the binary once it is built so we can vendor it in the buildpack.
+#
+# Once the dyno has is 'up' you can open your browser and navigate
+# this dyno's directory structure to download the nginx binary.
+
+NGINX_VERSION=${NGINX_VERSION-1.9.11}
+PCRE_VERSION=${PCRE_VERSION-8.38}
+ZLIB_VERSION=${ZLIB_VERSION-1.2.8}
+
+nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+pcre_tarball_url=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-${PCRE_VERSION}.tar.gz
+zlib_url=http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz
+
+temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
+
+echo "Serving files from /tmp on $PORT"
+cd /tmp
+python -m SimpleHTTPServer $PORT &
+
+cd $temp_dir
+echo "Temp dir: $temp_dir"
+
+echo "Downloading $nginx_tarball_url"
+curl -L $nginx_tarball_url | tar xzv
+
+echo "Downloading $pcre_tarball_url"
+(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvz )
+
+echo "Downloading $zlib_url"
+(cd nginx-${NGINX_VERSION} && curl -L $zlib_url | tar xvz )
+
+(
+  cd nginx-${NGINX_VERSION}
+  ./configure \
+    --with-pcre=pcre-${PCRE_VERSION} \
+    --with-zlib=zlib-${ZLIB_VERSION} \
+    --prefix=/tmp/nginx \
+    --with-http_gzip_static_module \
+    --with-http_ssl_module \
+    --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' \
+    --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed'
+
+  make install
+)
+
+while true
+do
+  sleep 1
+  echo "."
+done
